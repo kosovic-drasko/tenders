e154a4787ec165f9696fca34bbc986f02d3299ed
kosovic-drasko <kosovic.drasko@montefarm.co.me>
2022-10-02 00:21:17 +0200

    init

--- src/main/webapp/app/entities/entity-routing.module.ts
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -6,47 +6,47 @@ import { RouterModule } from '@angular/router';
     RouterModule.forChild([
       {
         path: 'ponude',
-        data: { pageTitle: 'tenderApp.ponude.home.title' },
+        data: { pageTitle: 'tenderiApp.ponude.home.title' },
         loadChildren: () => import('./ponude/ponude.module').then(m => m.PonudeModule),
       },
       {
         path: 'hvale-ponude',
-        data: { pageTitle: 'tenderApp.hvalePonude.home.title' },
+        data: { pageTitle: 'tenderiApp.hvalePonude.home.title' },
         loadChildren: () => import('./hvale-ponude/hvale-ponude.module').then(m => m.HvalePonudeModule),
       },
       {
         path: 'ponudjaci',
-        data: { pageTitle: 'tenderApp.ponudjaci.home.title' },
+        data: { pageTitle: 'tenderiApp.ponudjaci.home.title' },
         loadChildren: () => import('./ponudjaci/ponudjaci.module').then(m => m.PonudjaciModule),
       },
       {
         path: 'postupci',
-        data: { pageTitle: 'tenderApp.postupci.home.title' },
+        data: { pageTitle: 'tenderiApp.postupci.home.title' },
         loadChildren: () => import('./postupci/postupci.module').then(m => m.PostupciModule),
       },
       {
         path: 'specifikacije',
-        data: { pageTitle: 'tenderApp.specifikacije.home.title' },
+        data: { pageTitle: 'tenderiApp.specifikacije.home.title' },
         loadChildren: () => import('./specifikacije/specifikacije.module').then(m => m.SpecifikacijeModule),
       },
       {
         path: 'tenderi-home',
-        data: { pageTitle: 'tenderApp.tenderiHome.home.title' },
+        data: { pageTitle: 'tenderiApp.tenderiHome.home.title' },
         loadChildren: () => import('./tenderi-home/tenderi-home.module').then(m => m.TenderiHomeModule),
       },
       {
         path: 'vrednovanje',
-        data: { pageTitle: 'tenderApp.vrednovanje.home.title' },
+        data: { pageTitle: 'tenderiApp.vrednovanje.home.title' },
         loadChildren: () => import('./vrednovanje/vrednovanje.module').then(m => m.VrednovanjeModule),
       },
       {
         path: 'prvorangirani',
-        data: { pageTitle: 'tenderApp.prvorangirani.home.title' },
+        data: { pageTitle: 'tenderiApp.prvorangirani.home.title' },
         loadChildren: () => import('./prvorangirani/prvorangirani.module').then(m => m.PrvorangiraniModule),
       },
       {
         path: 'ponude_ponudjaci',
-        data: { pageTitle: 'tenderApp.ponudePonudjaci.home.title' },
+        data: { pageTitle: 'tenderiApp.ponudePonudjaci.home.title' },
         loadChildren: () => import('./ponude-ponudjaci/ponude-ponudjaci.module').then(m => m.PonudePonudjaciModule),
       },
       {
